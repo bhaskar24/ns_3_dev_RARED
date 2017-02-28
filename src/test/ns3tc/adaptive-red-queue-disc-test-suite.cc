4f1997ef295745299d22206f81b0c7c9b9483563
bhaskar24 <bhaskar.gautam2494@gmail.com>
2017-02-28 12:52:34 +0530

    Test Case for RARED

--- src/test/ns3tc/adaptive-red-queue-disc-test-suite.cc
+++ src/test/ns3tc/adaptive-red-queue-disc-test-suite.cc
@@ -457,6 +457,218 @@ AdaptiveRedQueueDiscTestCase::DoRun (void)
   Simulator::Destroy ();
 }
 
+/*
+ * Gautam RARED Test Case Start
+ *
+*/
+// Tests to verify the working of *adaptive* parameter in ARED
+class RefinedAdaptiveRedQueueDiscTestCase : public TestCase
+{
+public:
+  RefinedAdaptiveRedQueueDiscTestCase ();
+  virtual void DoRun (void);
+private:
+  void RunRefinedAdaptiveRedDiscTest (StringValue mode);
+};
+
+RefinedAdaptiveRedQueueDiscTestCase::RefinedAdaptiveRedQueueDiscTestCase ()
+  : TestCase ("Sanity check on adaptive parameter of RARED")
+{
+}
+
+void
+RefinedAdaptiveRedQueueDiscTestCase::RunRefinedAdaptiveRedDiscTest (StringValue mode)
+{
+  uint32_t    pktSize = 1000;
+  uint32_t    modeSize = 1;             // 1 for packets; pktSize for bytes
+  std::string aredLinkDataRate = "1.5Mbps";
+  std::string aredLinkDelay = "20ms";
+
+  double global_start_time = 0.0;
+  double global_stop_time = 7.0;
+  double sink_start_time = global_start_time;
+  double sink_stop_time = global_stop_time + 3.0;
+  double client_start_time = global_start_time + 1.5;
+  double client_stop_time = global_stop_time - 2.0;
+
+  NodeContainer n0n2;
+  NodeContainer n1n2;
+  NodeContainer n2n3;
+  NodeContainer n3n4;
+  NodeContainer n3n5;
+
+  Ipv4InterfaceContainer i0i2;
+  Ipv4InterfaceContainer i1i2;
+  Ipv4InterfaceContainer i2i3;
+  Ipv4InterfaceContainer i3i4;
+  Ipv4InterfaceContainer i3i5;
+
+  NodeContainer c;
+  c.Create (6);
+  n0n2 = NodeContainer (c.Get (0), c.Get (2));
+  n1n2 = NodeContainer (c.Get (1), c.Get (2));
+  n2n3 = NodeContainer (c.Get (2), c.Get (3));
+  n3n4 = NodeContainer (c.Get (3), c.Get (4));
+  n3n5 = NodeContainer (c.Get (3), c.Get (5));
+
+  Config::SetDefault ("ns3::TcpL4Protocol::SocketType", StringValue ("ns3::TcpNewReno"));
+  // 42 = headers size
+  Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (1000 - 42));
+  Config::SetDefault ("ns3::TcpSocket::DelAckCount", UintegerValue (1));
+  GlobalValue::Bind ("ChecksumEnabled", BooleanValue (false));
+
+  uint32_t meanPktSize = 1000;
+
+  Ptr<RedQueueDisc> queue = CreateObject<RedQueueDisc> ();
+
+  NS_TEST_EXPECT_MSG_EQ (queue->SetAttributeFailSafe ("Mode", mode), true,
+                         "Verify that we can actually set the attribute Mode");
+
+  Ipv4Header ipHeader;
+
+  if (queue->GetMode () == Queue::QUEUE_MODE_BYTES)
+    {
+      modeSize = pktSize + ipHeader.GetSerializedSize ();
+    }
+
+  uint32_t    qSize = 25 * modeSize;
+
+  Config::SetDefault ("ns3::RedQueueDisc::ARED", BooleanValue (false));
+  Config::SetDefault ("ns3::RedQueueDisc::RARED", BooleanValue (true));
+  Config::SetDefault ("ns3::RedQueueDisc::LInterm", DoubleValue (10.0));
+  Config::SetDefault ("ns3::RedQueueDisc::QueueLimit", UintegerValue (qSize));
+  Config::SetDefault ("ns3::RedQueueDisc::MeanPktSize", UintegerValue (meanPktSize + ipHeader.GetSerializedSize ()));
+  Config::SetDefault ("ns3::RedQueueDisc::TargetDelay", TimeValue (Seconds (0.2)));
+
+  InternetStackHelper internet;
+  internet.Install (c);
+
+  TrafficControlHelper tchPfifo;
+  uint16_t handle = tchPfifo.SetRootQueueDisc ("ns3::PfifoFastQueueDisc");
+  tchPfifo.AddInternalQueues (handle, 3, "ns3::DropTailQueue", "MaxPackets", UintegerValue (1000));
+
+  TrafficControlHelper tchRed;
+  tchRed.SetRootQueueDisc ("ns3::RedQueueDisc", "LinkBandwidth", StringValue (aredLinkDataRate),
+                           "LinkDelay", StringValue (aredLinkDelay));
+
+  PointToPointHelper p2p;
+
+  NetDeviceContainer devn0n2;
+  NetDeviceContainer devn1n2;
+  NetDeviceContainer devn2n3;
+  NetDeviceContainer devn3n4;
+  NetDeviceContainer devn3n5;
+
+  QueueDiscContainer queueDiscs;
+
+  p2p.SetQueue ("ns3::DropTailQueue");
+  p2p.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
+  p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  devn0n2 = p2p.Install (n0n2);
+  tchPfifo.Install (devn0n2);
+
+  p2p.SetQueue ("ns3::DropTailQueue");
+  p2p.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
+  p2p.SetChannelAttribute ("Delay", StringValue ("3ms"));
+  devn1n2 = p2p.Install (n1n2);
+  tchPfifo.Install (devn1n2);
+
+  p2p.SetQueue ("ns3::DropTailQueue");
+  p2p.SetDeviceAttribute ("DataRate", StringValue (aredLinkDataRate));
+  p2p.SetChannelAttribute ("Delay", StringValue (aredLinkDelay));
+  devn2n3 = p2p.Install (n2n3);
+  // only backbone link has ARED queue disc
+  queueDiscs = tchRed.Install (devn2n3);
+
+  p2p.SetQueue ("ns3::DropTailQueue");
+  p2p.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
+  p2p.SetChannelAttribute ("Delay", StringValue ("4ms"));
+  devn3n4 = p2p.Install (n3n4);
+  tchPfifo.Install (devn3n4);
+
+  p2p.SetQueue ("ns3::DropTailQueue");
+  p2p.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
+  p2p.SetChannelAttribute ("Delay", StringValue ("5ms"));
+  devn3n5 = p2p.Install (n3n5);
+  tchPfifo.Install (devn3n5);
+
+  Ipv4AddressHelper ipv4;
+  ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+  i0i2 = ipv4.Assign (devn0n2);
+  ipv4.SetBase ("10.1.2.0", "255.255.255.0");
+  i1i2 = ipv4.Assign (devn1n2);
+  ipv4.SetBase ("10.1.3.0", "255.255.255.0");
+  i2i3 = ipv4.Assign (devn2n3);
+  ipv4.SetBase ("10.1.4.0", "255.255.255.0");
+  i3i4 = ipv4.Assign (devn3n4);
+  ipv4.SetBase ("10.1.5.0", "255.255.255.0");
+  i3i5 = ipv4.Assign (devn3n5);
+
+  // Set up the routing
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  // SINK is in the right side
+  uint16_t port = 50000;
+  Address sinkLocalAddress (InetSocketAddress (Ipv4Address::GetAny (), port));
+  PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", sinkLocalAddress);
+  ApplicationContainer sinkApp = sinkHelper.Install (n3n4.Get (1));
+  sinkApp.Start (Seconds (sink_start_time));
+  sinkApp.Stop (Seconds (sink_stop_time));
+
+  // Connection one
+  // Clients are in left side
+  /*
+   * Create the OnOff applications to send TCP to the server
+   * onoffhelper is a client that send data to TCP destination
+  */
+  OnOffHelper clientHelper1 ("ns3::TcpSocketFactory", Address ());
+  clientHelper1.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+  clientHelper1.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+  clientHelper1.SetAttribute ("PacketSize", UintegerValue (pktSize));
+  clientHelper1.SetAttribute ("DataRate", DataRateValue (DataRate ("100Mbps")));
+
+  // Connection two
+  OnOffHelper clientHelper2 ("ns3::TcpSocketFactory", Address ());
+  clientHelper2.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+  clientHelper2.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+  clientHelper2.SetAttribute ("PacketSize", UintegerValue (pktSize));
+  clientHelper2.SetAttribute ("DataRate", DataRateValue (DataRate ("100Mbps")));
+
+  ApplicationContainer clientApps1;
+  AddressValue remoteAddress (InetSocketAddress (i3i4.GetAddress (1), port));
+  clientHelper1.SetAttribute ("Remote", remoteAddress);
+  clientApps1.Add (clientHelper1.Install (n0n2.Get (0)));
+  clientApps1.Start (Seconds (client_start_time));
+  clientApps1.Stop (Seconds (client_stop_time));
+
+  ApplicationContainer clientApps2;
+  clientHelper2.SetAttribute ("Remote", remoteAddress);
+  clientApps2.Add (clientHelper2.Install (n1n2.Get (0)));
+  clientApps2.Start (Seconds (client_start_time));
+  clientApps2.Stop (Seconds (client_stop_time));
+
+  Simulator::Stop (Seconds (sink_stop_time));
+  Simulator::Run ();
+
+  RedQueueDisc::Stats st = StaticCast<RedQueueDisc> (queueDiscs.Get (0))->GetStats ();
+//  NS_TEST_EXPECT_MSG_LT (st.unforcedDrop, st.forcedDrop, "forcedDrop should be more than unforcedDrop");
+  NS_TEST_EXPECT_MSG_NE (st.unforcedDrop, st.forcedDrop, "forcedDrop should be less than unforcedDrop");
+
+  Simulator::Destroy ();
+}
+
+void
+RefinedAdaptiveRedQueueDiscTestCase::DoRun (void)
+{
+  RunRefinedAdaptiveRedDiscTest (StringValue ("QUEUE_MODE_PACKETS"));
+  RunRefinedAdaptiveRedDiscTest (StringValue ("QUEUE_MODE_BYTES"));
+  Simulator::Destroy ();
+}
+
+/*
+ * Gautam RARED Test Case End
+*/
+
 static class AredQueueDiscTestSuite : public TestSuite
 {
 public:
@@ -465,5 +677,6 @@ public:
   {
     AddTestCase (new AutoRedQueueDiscTestCase (), TestCase::QUICK);         // Tests for automatically set parameters of ARED
     AddTestCase (new AdaptiveRedQueueDiscTestCase (), TestCase::QUICK);     // Tests for adaptive parameter of ARED
+    AddTestCase (new RefinedAdaptiveRedQueueDiscTestCase (), TestCase::QUICK);     // Tests for adaptive parameter of ARED
   }
 } g_aredQueueDiscTestSuite;
